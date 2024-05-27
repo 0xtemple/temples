7ce66c38a5a7e95f4aa4e01ae9807768204edf9a
web3olalala <w13225232270@163.com>
2024-05-27 23:46:20 +0800

    Add ProgramGenerator

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -5,17 +5,25 @@ mod systems;
 
 use crate::shemas::{CounterMap, CounterValue};
 use counter_metadata::SystemAction;
+use gstd::prog::ProgramGenerator;
 use gstd::{msg, ActorId};
 use hex_literal::hex;
 use temple_storage::nexcore_manager;
 
 #[gstd::async_init]
 async fn init() {
-    nexcore_manager::new(ActorId::from(hex![
-        "9cd7aeec68189c75b22b1778d221dd4c578ce425282cb42bc53861bb204184fb"
-    ]));
-    CounterValue::register(nexcore_manager::get()).await;
-    CounterMap::register(nexcore_manager::get()).await;
+    // ProgramGenerator returs ProgramId
+    let (_, nexcore) = ProgramGenerator::create_program_with_gas(
+        hex!("725d96aa0add2da5285ae9fee22017ba8873ec7b63757655304be3a4cce16760").into(),
+        b"",
+        10_000_000_000,
+        0,
+    )
+    .expect("Unable to create program");
+
+    nexcore_manager::new(nexcore);
+    CounterValue::register(nexcore).await;
+    CounterMap::register(nexcore).await;
 }
 
 #[gstd::async_main]
